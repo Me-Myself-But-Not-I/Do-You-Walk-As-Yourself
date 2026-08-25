570e8766ea53df7300d4bf07482c374c96df598f
NewGameDev <jordansamuel893@gmail.com>
2026-08-25 20:41:04 +1000

    Added updated scriptable objects

--- scripts/Scriptables/Details.cs
+++ scripts/Scriptables/Details.cs
@@ -3,9 +3,9 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "Details", menuName = "Scriptable Objects/Details")]
 public class Details : ScriptableObject
 {
-    public int[] person;
-    public string[] personNames;
     public bool[] found;
+    public int noFound;
+    public bool[] faePerson;
     public int day;
     
 }
